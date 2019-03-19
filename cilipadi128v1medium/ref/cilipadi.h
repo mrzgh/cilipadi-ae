c4bd6d54c14c0b307063ac8fb9eb7e0d613345e9
Muhammad Reza Z'aba <mrz@mRzs-MacBook-Pro.local>
2019-03-19 23:56:11 +0800

    revise encrypt.c and cilipadi.cand add medium, hot and extrahot

---
+++ cilipadi128v1medium/ref/cilipadi.h
@@ -0,0 +1,21 @@
+/*
+ * cilipadi.h
+ *
+ *  Created on: 25 Feb 2019
+ *      Author: mrz
+ */
+
+#ifndef CILIPADI128V1_REF_CILIPADI_H_
+#define CILIPADI128V1_REF_CILIPADI_H_
+
+#define BYTERATE 12 // bitrate in bytes
+#define AROUNDS 20 // number of rounds for P_{a,n}
+#define BROUNDS 18 // number of rounds for P_{b,n}
+#define STATELEN 32 // state size in bytes
+
+int permutation_n(unsigned char *state, int rounds);
+int permutation_n_inv(unsigned char *state, int rounds);
+int f_function(unsigned char *x);
+int xor_bytes(unsigned char *x, const unsigned char *y, int len);
+
+#endif /* CILIPADI128V1_REF_CILIPADI_H_ */
